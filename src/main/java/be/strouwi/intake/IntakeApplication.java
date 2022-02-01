542ef3072c766d0326611e0fe41086abd5dca366
Wim Strouven <wim.strouven@strouwi.be>
2022-02-01 23:14:59 +0100

    initial commit

---
+++ src/main/java/be/strouwi/intake/IntakeApplication.java
@@ -0,0 +1,13 @@
+package be.strouwi.intake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntakeApplication.class, args);
+	}
+
+}
